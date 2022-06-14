b1c5ce472b9801d1e783220888a32d42ac5d44ba
georgi.filipov <georgi.filipov@leverx.com>
2022-06-14 16:59:47 +0300

    fix

--- middleware/index.js
+++ middleware/index.js
@@ -3,8 +3,7 @@ const orders = require("./orders");
 const auth = require("./auth");
 const login = require("./login");
 const register = require("./register");
-const errorHandler = require("./errorHandler");
 
-const middlewares = [auth, login, register, ...users, ...orders, errorHandler];
+const middlewares = [auth, login, register, ...users, ...orders];
 
 module.exports = middlewares;
