70721178e58ccf48bf25effcf393954e095f8517
georgi.filipov <georgi.filipov@leverx.com>
2022-06-29 15:13:44 +0300

    fix bug

--- middleware/index.js
+++ middleware/index.js
@@ -3,6 +3,24 @@ const orders = require("./orders");
 const auth = require("./auth");
 const login = require("./login");
 const register = require("./register");
+// const { router } = require("../config");
+//
+// const ownersIds = [0, 20, 40, 60, 80];
+// const managersIds = [1, 7, 23, 36, 55, 60, 68, 81, 93, 96];
+//
+// const emp = router.db.get('employees').value().map(({ relatedId, ...el }) => el);
+//
+// const owners = emp.filter((_, idx) => ownersIds.includes(idx));
+// const managers = emp.filter((_, idx) => managersIds.includes(idx)).map((el, idx) => ({
+//   ...el,
+//   relatedId: emp[ownersIds[Math.floor(idx / 2)]].uuid,
+// }));
+// const employees = emp.filter((_, idx) => ![...ownersIds, ...managersIds].includes(idx)).map((el, idx) => ({
+//   ...el,
+//   relatedId: emp[Math.floor(Math.random() * managersIds.length)].uuid,
+// }));
+//
+// router.db.set('employees', [...employees, ...managers, ...owners]).write();
 
 const middlewares = [auth, login, register, ...users, ...orders];
 
